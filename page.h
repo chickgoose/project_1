be6fb46d0d754b1b6f53f04b7a0baefd335c445b
chickgoose <128354115+chickgoose@users.noreply.github.com>
2023-05-15 04:58:52 +0900

    Delete page.h

--- page.h
+++
@@ -1,9 +0,0 @@
-class Page {
-    public:
-
-    private:
-        int x, y; // position of the page on the board
-        int width, height; // width and height of the page 
-        int id; // unique id for each page
-        char content; 
-};
